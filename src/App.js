e64a4ea3800958466c0e709339b2c489e4122f8c
Safonovnn <safonovnn89@gmail.com>
2023-08-27 13:33:19 +0500

    sdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import imgSrc from "../public/img/lamborghini.jpg";
+import img from "./img/";
 
 function App() {
   return (
@@ -18,8 +18,7 @@ function App() {
         </ul>
       </nav>
       <div>
-        <img src={imgSrc} />
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgradskiportal018.rs%2F2021%2F08%2F05%2Fsto-da-ne-kazem-more-je-more-svuda%2F&psig=AOvVaw05qzuRTrmhk4OFt4LI61ex&ust=1693207115504000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMjU6Jam_IADFQAAAAAdAAAAABAJ" />
+        <img src={img} />
       </div>
     </div>
   );
